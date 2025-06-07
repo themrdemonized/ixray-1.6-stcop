0ad0d60f42bf06c01478192c42a5a6fed9c41332
v2v3v4 <terran.erre@mail.ru>
2025-06-08 00:29:09 +0300

    так будет меньше блокировок

--- src/xrGame/entity_alive.cpp
+++ src/xrGame/entity_alive.cpp
@@ -817,6 +817,7 @@ void CEntityAlive::OnChangeVisual					( )
 
 void CEntityAlive::fill_hit_bone_surface_areas		( ) const
 {
+	xrSRWLockGuard guard(&m_hit_bone_lock, true);
 	VERIFY								( !m_hit_bone_surface_areas_actual );
 	m_hit_bone_surface_areas_actual		= true;
 
@@ -878,10 +879,10 @@ Fvector	CEntityAlive::get_new_local_point_on_mesh	( u16& bone_id ) const
 	if ( !kinematics->LL_BoneCount() )
 		return							inherited::get_new_local_point_on_mesh( bone_id );
 
-	xrCriticalSectionGuard guard(&m_hit_bone_cs);
 	if ( !m_hit_bone_surface_areas_actual )
 		fill_hit_bone_surface_areas		( );
 
+	xrSRWLockGuard guard(&m_hit_bone_lock, false);
 	if ( m_hit_bone_surface_areas.empty() )
 		return							inherited::get_new_local_point_on_mesh( bone_id );
 
