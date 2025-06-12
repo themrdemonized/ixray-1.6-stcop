63d2c933c5ba03135c087cb90c7c40131013b56f
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-06-12 18:03:21 +0300

    Link `xrGameSOC` to `xrUI` (kinda buggy)

--- src/xrUI/Widgets/UIFrameLineWnd.h
+++ src/xrUI/Widgets/UIFrameLineWnd.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "UIWindow.h"
+#include "UIStatic.h"
 
 class UI_API CUIFrameLineWnd: 
 	public CUIWindow
@@ -34,6 +35,9 @@ public:
 				R_ASSERT(idx >= flFirst && idx <= flSecond);
 				m_tex_rect[idx] = rect;
 			}
+			// Also we can display textual caption on the frame
+			CUIStatic UITitleText;
+			CUIStatic* GetTitleStatic() { return &UITitleText; };
 
 protected:
 	bool			bHorizontal;
